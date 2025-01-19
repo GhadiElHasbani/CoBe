fixed attack angle calculation and plotting
also added smoothing option to attack angle function

round.py
@@ -194,16 +194,11 @@ class Round:
 
     def compute_acceleration(self, datas_vel: List[NDArray[float]],
                              smooth: bool = True, smoothing_args: Dict = None) -> List[NDArray[float]]:
+        datas_acc = [np.insert(np.diff(data_vel) / (np.diff(self.timestamps)), 0, [0., ]) for data_vel in datas_vel]
         if smooth:
-            if smoothing_args is None:
-                smoothing_args = {'smoothing_method': 'window',
-                                  'kernel': lambda x, i, b: 1,
-                                  'window_size': 40}
-            datas_acc = [np.insert(smooth_array(np.diff(data_vel) / (np.diff(self.timestamps)), **smoothing_args), 0, [0.,]) for data_vel in datas_vel]
-        else:
-            datas_acc = [np.insert(np.diff(data_vel) / (np.diff(self.timestamps)), 0, [0.,]) for data_vel in datas_vel]
+            datas_acc = smooth_metric(datas=datas_acc, smoothing_args=smoothing_args)
 
-        return datas_acc
+        return [np.insert(data_acc, 0, [0., ]) for data_acc in datas_acc]
 
     def compute_agent_acceleration(self, smooth: bool = True, smoothing_args: Dict = None) -> List[NDArray[float]]:
         agent_datas_vel = self.compute_agent_speed()
@@ -236,10 +231,12 @@ class Round:
 
         return pred_dist_to_center
 
-    def compute_predator_attack_angle(self) -> List[NDArray[float]]:
-        pred_attack_angles = [180 - compute_angle(self.agent_com[:-1], pred_data_arr[:-1], pred_data_arr[1]) for pred_data_arr in self.pred_datas_arr]
+    def compute_predator_attack_angle(self, smooth: bool = True, smoothing_args: Dict = None) -> List[NDArray[float]]:
+        preds_attack_angles = [compute_angle(self.agent_com[:-1], pred_data_arr[:-1], pred_data_arr[1]) for pred_data_arr in self.pred_datas_arr]
+        if smooth:
+            preds_attack_angles = smooth_metric(datas=preds_attack_angles, smoothing_args=smoothing_args)
 
-        return pred_attack_angles
+        return [np.insert(pred_attack_angles, len(pred_attack_angles), [0., ]) for pred_attack_angles in preds_attack_angles]
 
     def segment_into_bouts(self,
                            dist_tolerance: float = 0.9,

round_plotter.py
@@ -100,11 +100,16 @@ class RoundPlotter:
 
     def update_metric_ax_specs(self, data_list: List[NDArray[float]], ax: plt.Axes,
                                t: int, t_start: int, time_window_dur: float,
-                               metric: str, max_abs_val: float = None):
-        if max_abs_val is None:
-            max_abs_val = np.nanmax([np.nanmax(abs(data)) for data in data_list])
-        ax.set_ylim(np.nanmax([np.nanmin([np.nanmin(data[t_start:t + 1]) for data in data_list]), -max_abs_val]),
-                    np.nanmin([np.nanmax([np.nanmax(data[t_start:t + 1]) for data in data_list]), max_abs_val]))
+                               metric: str, max_abs_val: float = None, keep_y_axis_stable: bool = False):
+
+        if keep_y_axis_stable:
+            ax.set_ylim(np.nanmin([np.nanmin(data) for data in data_list]),
+                        np.nanmax([np.nanmax(data) for data in data_list]))
+        else:
+            if max_abs_val is None:
+                max_abs_val = np.nanmax([np.nanmax(abs(data)) for data in data_list])
+            ax.set_ylim(np.nanmax([np.nanmin([np.nanmin(data[t_start:t + 1]) for data in data_list]), -max_abs_val]),
+                        np.nanmin([np.nanmax([np.nanmax(data[t_start:t + 1]) for data in data_list]), max_abs_val]))
         ax.set_xlim(self.round.timestamps[t] - 2 * time_window_dur / 3,
                     self.round.timestamps[t] + time_window_dur / 3)
         ax.set_xlabel("Time [s]")
@@ -171,12 +176,15 @@ class RoundPlotter:
                                         t=t, t_start=t_start, time_window_dur=time_window_dur,
                                         metric="distance from closest border")
             # Predator distance to border axes
+            self.update_predator_lines(pred_datas_att_smoothed, ax=args_dict['ax_att'],
+                                       lines=[args_dict[f'att_smoothed_{pid}'] for pid in range(self.round.n_preds)],
+                                       t=t, t_start=t_start)
             self.update_predator_lines(pred_datas_att, ax=args_dict['ax_att'],
                                        lines=[args_dict[f'att_{pid}'] for pid in range(self.round.n_preds)],
-                                       t=t, t_start=t_start)
+                                       t=t, t_start=t_start, linestyle='--', alpha=0.6)
             self.update_metric_ax_specs(pred_datas_att, ax=args_dict['ax_att'],
                                         t=t, t_start=t_start, time_window_dur=time_window_dur,
-                                        metric="attack angle")
+                                        metric="attack angle", keep_y_axis_stable=True)
 
             return args_dict
 
@@ -197,7 +205,8 @@ class RoundPlotter:
         pred_datas_acc = self.round.compute_predator_acceleration(smooth=False)
         pred_datas_com = self.round.compute_predator_distance_to_agent_com()
         pred_datas_bor = self.round.compute_predator_distance_to_border()
-        pred_datas_att = self.round.compute_predator_attack_angle()
+        pred_datas_att_smoothed = self.round.compute_predator_attack_angle(smoothing_args=smoothing_args)
+        pred_datas_att = self.round.compute_predator_attack_angle(smooth=False)
 
         # speed
         ax_vel = fig.add_subplot(gs[0, 1])
@@ -223,6 +232,7 @@ class RoundPlotter:
             args_dict[f'acc_smoothed_{pid}'], = ax_acc.plot([], [], color=self.colors[pid][:-1])
             args_dict[f'com_{pid}'], = ax_com.plot([], [], color=self.colors[pid][:-1])
             args_dict[f'bor_{pid}'], = ax_bor.plot([], [], color=self.colors[pid][:-1])
+            args_dict[f'att_smoothed_{pid}'], = ax_att.plot([], [], color=self.colors[pid][:-1])
             args_dict[f'att_{pid}'], = ax_att.plot([], [], color=self.colors[pid][:-1])
 
         ani = animation.FuncAnimation(fig=fig, func=update, frames=len(self.round.timestamps), interval=1, fargs=(args_dict,))